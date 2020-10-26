2fa9d96a473e90be0e1d7bf7e0a02e54d0584a00
HappyGod <chami0205@gmail.com>
2020-10-26 13:30:20 +0900

    1

---
+++ CULG/CULG/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CULG
+//
+//  Created by 김우일 on 2020/04/02.
+//  Copyright © 2020 김우일. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
